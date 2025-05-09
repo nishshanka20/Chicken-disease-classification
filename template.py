ea51c2950095c4ae327cce15dc6b0893a3235dff
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2025-05-09 08:43:14 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:  %(message)s:')
+
+project_name = "chicken-disease-prediction"
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py", 
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trial.ipynb"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) #windows use backslash path. so set path is important.
+    filedir,filename=os.path.split(filepath) #split filename and folder name
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory;{filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}" )
+    
+    else:
+        logging.info(f"{filename} is already exists")
+
+
+
