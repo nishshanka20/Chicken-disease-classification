a160365ee16bfb67be6d677d60ec4adad6efa852
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2025-05-27 07:56:38 +0530

    prepare callbacks added to code

--- src/chicken_disease_prediction/config/configuration.py
+++ src/chicken_disease_prediction/config/configuration.py
@@ -1,6 +1,7 @@
 from chicken_disease_prediction.constants import *
+import os
 from chicken_disease_prediction.utils.common import read_yaml,create_directories
-from chicken_disease_prediction.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from chicken_disease_prediction.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,PrepareCallbacksConfig)
 
 
 class ConfigurationManager:
@@ -44,4 +45,20 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_prepare_callbacks_config(self) -> PrepareCallbacksConfig:
+        config=self.config.prepare_callbacks
+        model_ckpt_dir=os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+             Path(model_ckpt_dir),
+             Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
     
